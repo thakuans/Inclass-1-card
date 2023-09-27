41ba9090bfb2763c56bb19b31f99dd696f9bc470
thakuans <thakuans@sheridancollege.ca>
2023-09-27 14:46:07 -0400

    this is my code

--- src/card/Card.java
+++ src/card/Card.java
@@ -11,7 +11,8 @@ package card;
  * There are 52 cards in a deck, no jokers.
  * This code is to be used in ICE1. When you create your own branch,
  * add your name as a modifier.
- * @author srinivsi
+ * @author Anshika Thakur
+ * Student id 991698396
  */
 public class Card {
 
@@ -46,7 +47,18 @@ public class Card {
     public void setValue(int value) {
         this.value = value;
     }
+    public boolean equals(Object obj){
+        if(this == obj){
+            return true;
+        }
+        if(obj == null || getClass()!= obj.getClass()){
+            return false;
+        }
+        Card otherCard = (Card) obj;
+        return value == otherCard.value && suit.equals(otherCard.suit);
+        
+        }
+    }
    
    
     
-}
--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -8,7 +8,8 @@ package card;
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
- * @author srinivsi
+ * @author anshika Thakur
+ * Student number 991698396
  */
 public class CardTrick {
     
@@ -19,14 +20,31 @@ public class CardTrick {
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
+            c.setValue((int)(Math.random()*13+1));
+            c.setSuit(Card.SUITS[(int)(Math.random()*4)]);
+            magicHand[i] = c;
             //c.setValue(insert call to random number generator here)
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
         }
         
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
-        // add one luckcard hard code 2,clubs
+       Card luckyCard = new Card();
+       luckyCard.setValue(3);
+       luckyCard.setSuit("Spades");
+       
+       boolean found = false;
+       for (int i= 0;i<magicHand.length;i++){
+           if (magicHand[i].equals(luckyCard)){
+               found = true;
+               break;
+           }
+       }
+    if(found){
+        System.out.println("Congratulation!toubgot lucky card");
+    }else{
+        System.out.println("Soory! better luck next time");
+    }
+    }
     }
     
-}
+    
+
